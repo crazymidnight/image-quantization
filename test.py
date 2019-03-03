fe65b06337ada41420cffeec9ef6a8b64e4da98f
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2019-03-03 14:18:51 +0700

    change benchmark

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import subprocess
 
-with open("logs.txt") as f:
-    for i in range(2, 12, 2):
-        for j in range(3):
-            subprocess.call(["./quant", f"{i}"])
+
+for i in range(10):
+    subprocess.call(["./quant", "1"])
+for i in range(2, 14, 2):
+    for j in range(10):
+        subprocess.call(["./quant", f"{i}"])
