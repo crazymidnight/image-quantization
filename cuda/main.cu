83a62ab7f7f69d7432829933aca3496a44a4b06a
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2019-04-18 13:53:02 +0700

    rename main.cpp to main.cu

--- cuda/main.cu
+++ cuda/main.cu
@@ -1,9 +1,7 @@
-#define N 65536 // N is Dimension of vectors
 #include <iostream>
 #include <string>
 #include <cstdlib>
 #include <math.h>
-#include <cuda.h>
 
 #include "../utils/bitmap_image.hpp"
 
@@ -57,17 +55,14 @@ int main(int argc, char *argv[]) {
 	unsigned char r, g, b;
 	unsigned int w = image.width();
 	unsigned int h = image.height();
-	unsigned char *red = new unsigned char[w*h];
-	unsigned char *green = new unsigned char[w*h];
-	unsigned char *blue = new unsigned char[w*h];
 	unsigned char *intensity = new unsigned char[w*h];
+
+	cudaSetDevice(0);
+	cudaCheckErrord();
 
 	for (int i = 0; i < w; i++) {
 		for (int j = 0; j < h; j++) {
 			image.get_pixel(i, j, r, g, b);
-			red[j*w + i] = r;
-			green[j*w + i] = g;
-			blue[j*w + i] = b;
 			int avg = ((int)r + (int)g + (int)b) / 3;
 			intensity[j*w + i] = (unsigned char)avg;
 		}
@@ -84,9 +79,6 @@ int main(int argc, char *argv[]) {
 	}
 	
 	G.save_image("output.bmp");
-	free(red);
-	free(green);
-	free(blue);
 	free(intensity);
 	printf("Execution time: %f\n");
 	return 0;
